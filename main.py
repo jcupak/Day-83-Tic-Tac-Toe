61a9dc19d2d7ed9795238fa827cbc62b83d7c0d2
John Cupak <john.cupak@me.com>
2021-05-26 14:03:18 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Day 83 Tic-Tac-Toe
+# Portfolio Python Solution
+# Author: John Cupak
+
+def game():
+    """Play game of tic-tac-toe with computer"""
+    pass
+
+
+if __name__ == '__main__':
+    game()
